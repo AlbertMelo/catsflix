51f0f76040ad188603b3589c9bd4d0357b29ce8b
AlbertMelo <menos.albert1981@gmail.com>
2020-07-31 21:15:58 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
-import CadastroVideo from './pages/cadastro/Video'
+import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
-import Erro404 from './pages/Erro404'
+import Erro404 from './pages/Erro404';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -15,15 +15,15 @@ ReactDOM.render(
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
-      
+
       <Route component={Erro404} />
 
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+// serviceWorker.unregister();
