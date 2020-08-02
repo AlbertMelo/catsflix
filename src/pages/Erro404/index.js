7e4f979c67e75d76a740ba28c9c26a68f1daf975
AlbertMelo <menos.albert1981@gmail.com>
2020-08-02 19:00:59 -0400

    Alterando 404

--- src/pages/Erro404/index.js
+++ src/pages/Erro404/index.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import PageDefault from '../../components/PageDefault';
-import Logo404 from '../../assets/img/Logo404.jpg'
+import Logo404 from '../../assets/img/Logo404.jpg';
+import './index.css';
 
 function Erro404() {
-    return (
-        <PageDefault>
-            <div>
-                <table>
-                    <tr>
-                        <th >
-                            <img src={Logo404} alt="Logo Erro 404"></img>
+  return (
+    <PageDefault>
+      <div>
 
-                        </th>
-                        <th>
-                            
-                            <p>
-                                Humano!!! </p>
-                            <p>A página que você quer acessar não existe!!!
-                            Vai encher meu pote de ração!
-                         </p>
+        <img className="imagem404" src={Logo404} alt="Logo Erro 404" />
 
-                        </th>
-                    </tr>
-                </table>
+        <h1 className="titulo">
+          Humano!!!
+        </h1>
+        <p className="conteudo_miau">
+          Miau!
+        </p>
+        <p className="conteudo">
+          Não existe a página que você está procurando!
+        </p>
+        <br />
+        <br />
+        <p className="conteudo">
+          Aproveita e enche meu pode de ração!
+        </p>
 
-
-
-            </div>
-        </PageDefault>
-    );
+      </div>
+    </PageDefault>
+  );
 }
 
 export default Erro404;
